f84ecb348843149b22571f5c58f1693e8c621c49
jalecf6 <112406967+Jacob-Lee-Student@users.noreply.github.com>
2023-03-14 12:20:51 +0000

    Finished

---
+++ Assets/Delivery.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] float destroyDelay;
+    bool hasPackage;
+    SpriteRenderer spriteRenderer;
+    private void Start() {
+       spriteRenderer = GetComponent<SpriteRenderer>(); 
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("Collision");
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Package" && !hasPackage) {
+            Debug.Log("Packaged picked up");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
+        }
+        if (other.tag == "Customer" && hasPackage) {
+            Debug.Log("Package delivered");
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
+        }
+    }
+}
--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 0.1f;
-    [SerializeField] float moveSpeed = 0.001f;
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 25f;
     
     void Update()
     {
@@ -19,4 +16,13 @@ public class Driver : MonoBehaviour
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
     }
+    private void OnCollisionEnter2D(Collision2D other) {
+        moveSpeed = slowSpeed;
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+     if(other.tag == "Boost") {
+        moveSpeed = boostSpeed;
+     }  
+    }
 }
+
